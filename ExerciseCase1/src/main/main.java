865860267b5a4ce32f619ea419a31deb24866151
Faris Adam <farisadam97@gmail.com>
2022-03-01 15:31:04 +0700

    add create iterative person object

--- ExerciseCase1/src/main/main.java
+++ ExerciseCase1/src/main/main.java
@@ -1,4 +1,19 @@
 package main;
 
+import model.person;
+
 public class main {
+    public static void main(String[] args) {
+        String[] listName = {"Robert","Downey","Chris","Tony"};
+
+        person [] listPerson = new person[listName.length];
+
+        for (int i = 0; i < listName.length; i++) {
+            listPerson[i] = new person(listName[i],i,0);
+        }
+
+        for (person human: listPerson) {
+            System.out.println(human.getName());
+        }
+    }
 }
