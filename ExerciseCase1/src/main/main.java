24cd36fdd4e0c61a542715cbd3724f6ccb30a3fa
Naufal <naufal.ti@outlook.com>
2022-03-01 16:49:36 +0700

    Adding transaction

--- ExerciseCase1/src/main/main.java
+++ ExerciseCase1/src/main/main.java
@@ -2,18 +2,51 @@ package main;
 
 import model.person;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class main {
+    public static String[] history;
     public static void main(String[] args) {
         String[] listName = {"Robert","Downey","Chris","Tony"};
+
 
         person [] listPerson = new person[listName.length];
 
         for (int i = 0; i < listName.length; i++) {
-            listPerson[i] = new person(listName[i],i,0);
+            listPerson[i] = new person(listName[i],i,50);
         }
 
-        for (person human: listPerson) {
+        getAllUserName(listPerson);
+
+
+    }
+    public static void getAllUserName(person[] person){
+        for (person human: person) {
             System.out.println(human.getName());
         }
+    }
+    public static void transaction(person sender, person receiver, int amount){
+        // check balance
+        int senderPoint = sender.getPoint();
+        if (senderPoint < amount){
+            System.out.println("Insufficient Sender's Point");
+            return;
+        }
+        // reduce sender balance
+        sender.setPoint(senderPoint-amount);
+
+        // increase receiver balance
+        int receiverPoint = receiver.getPoint();
+        receiver.setPoint(receiverPoint+amount);
+
+        // Record to transaction History
+        Date date = new Date();
+        SimpleDateFormat formatter = new SimpleDateFormat("dd/MM/yyyy");
+        String strDate = formatter.format(date);
+
+        //TODO fix command append list
+        //main.history.add(strDate+sender.getName()+String.valueOf(amount)+receiver.getName());
+
     }
 }
