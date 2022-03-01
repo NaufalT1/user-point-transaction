25ead4466d40eaaa24912f9c5f2fdb12a43af03e
Faris Adam <farisadam97@gmail.com>
2022-03-01 15:16:16 +0700

    add person class detail

--- ExerciseCase1/src/model/person.java
+++ ExerciseCase1/src/model/person.java
@@ -1,4 +1,37 @@
 package model;
 
 public class person {
+    String name;
+    Integer id;
+    Integer point;
+
+    public person(String name, Integer id, Integer point) {
+        this.name = name;
+        this.id = id;
+        this.point = point;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getPoint() {
+        return point;
+    }
+
+    public void setPoint(Integer point) {
+        this.point = point;
+    }
 }
